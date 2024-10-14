9063dc6d1ff9c9663564ff752903f4626d40af48
Luiz Gabriel dos Santos <106356842+Lgsantos1908@users.noreply.github.com>
2024-10-14 13:04:41 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,65 +10,21 @@ import { useRouter } from 'next/router';
 
 
 function Home() {
-
-  const router = useRouter(); 
-
-  const handleClink = () => {
-    router.push('page')
-  }
-
-const buttonRef = useRef(null);
-    const [buttonPosition, setButtonPosition] = useState({top: 200, left: 200});
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      setCursorPosition({ x: event.clientX, y: event.clientY });
-    };
-
-    document.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (buttonRef.current) {
-      const rect = buttonRef.current.getBoundingClientRect();
-      const buffer = 20; // Buffer de distância entre o cursor e o botão
-
-      if (
-        cursorPosition.x > rect.left - buffer &&
-        cursorPosition.x < rect.right + buffer &&
-        cursorPosition.y > rect.top - buffer &&
-        cursorPosition.y < rect.bottom + buffer
-
-        
-      ) {
-        // Se o cursor estiver dentro do buffer, move o botão para uma nova posição aleatória
-        const newTop = Math.random() * (window.innerHeight - rect.height);
-        const newLeft = Math.random() * (window.innerWidth - rect.width);
-        setButtonPosition({ top: newTop, left: newLeft });
-      }
-    }
-  }, [cursorPosition]);
-   
-
+    
     return (
         <>
             <GlobalStyle/>
             <Container>
                 <Page>
                     
-                    <img src='https://github.com/Lgsantos1908/lista_Carros/blob/main/WhatsApp%20Image%202024-08-22%20at%2010.01.16.png?raw=true' alt='Logo restaurante'/>
-                    <h1>Olá, Chefe</h1>
-                    <h2>Poderia considerar me conceder um aumento, por favor?</h2>
-                    <Butons>
-                          <button onClick={handleClink}>CERTAMENTE! VOCÊ É MEU MELHOR COLABORADOR.</button>
-                        <StyledButton  ref={buttonRef} top={buttonPosition.top} left={buttonPosition.left}>NÃO</StyledButton >
-                        
-                    </Butons>
+                   <div class="card-item">
+                    <img src="./images/car-card.png" alt="Car">
+                    <div class="card-content">
+                        <h3>Audi Supra TT</h3>
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae!</p>
+                        <button type="button">I want This!</button>
+                    </div>
+                </div>  
                 </Page>
             </Container>
         </>
